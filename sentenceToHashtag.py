2c6d163edbbcff917e98662f82fbdd12a7f5416b
TxeMac <txema.bermudez@gmail.com>
2016-04-01 22:04:42 +0100

    solution

---
+++ sentenceToHashtag.py
@@ -0,0 +1,24 @@
+from string import ascii_letters
+
+
+__author__ = "josebermudez"
+
+
+def sentence_to_hashtag(sentence):
+    """
+    This function convert a sentence in a hashtag.
+    Non-alphabetic characters are ignored.
+    E.g. 'thi9s Is &&So cool' -> '#ThisIsSoCool'.
+    :param sentence: sentence
+    :return: hashtag
+    """
+    result = '#'
+
+    for word in sentence.split():
+        # clean non-alphabetic characters
+        aux = "".join([ch for ch in word if ch in ascii_letters])
+
+        # add capitalize word
+        result += aux.title()
+
+    return result
